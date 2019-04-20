597e516a674bd374a5aa594cd23df0166c41c27d
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-04-21 02:03:36 +0800

    Add navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import AppContainer from './components/DesignComponents/AppStyles'
+import Header from './components/SharedComponents/Header'
 import Routes from './components/RouteComponents/Routes'
+
 
 const App = () => {
   return (
     <AppContainer>
+      <Header />
       <Routes />
     </AppContainer>
   )
